a7df77c99dc79c9c55d3bd5682438a0fc722e87c
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-15 02:20:09 +0900

    feat: login.php 추가

---
+++ views/user/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인</title>
+    <link rel="stylesheet" href="/public/css/bootstrap.css">
+</head>
+
+<body>
+    <div class="mb-3">
+        <label for="" class="form-label">Name</label>
+        <input type="text" class="form-control" name="" id="" aria-describedby="helpId" placeholder="" />
+        <small id="helpId" class="form-text text-muted">Help text</small>
+    </div>
+
+    <?php
+    include '/footer.php';
+    ?>
+</body>
+
+</html>
