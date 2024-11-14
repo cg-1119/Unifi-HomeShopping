be15835339c13ba7c419d62def2f8eb73690f05e
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-15 02:34:25 +0900

    chore: custom-style.css로 스타일 이동, 링크 추가

--- views/home/footer.php
+++ views/home/footer.php
@@ -1,5 +1,6 @@
 <!-- views/home/footer.php -->
-<!-- 푸터 -->
+<link rel="stylesheet" href="/public/css/custom-style.css">
+
 <footer class="bg-dark text-white text-center py-3">
     <div class="container">
         <p class="mb-0">copyright, 고객센터 링크</p>
--- views/home/header.php
+++ views/home/header.php
@@ -1,21 +1,12 @@
 <!-- views/home/header.php -->
-<style>
-    .navbar {
-        position: relative;
-    }
+<link rel="stylesheet" href="/public/css/custom-style.css">
 
-    .navbar-brand {
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-    }
-</style>
 <header>
     <nav class="navbar navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container d-flex justify-content-end">
             <a class="navbar-brand mx-auto" href="/views/home/index.php">쇼핑몰</a>
             <div class="d-flex gap-3">
-                <a class="nav-link text-white" href="#">로그인</a>
+                <a class="nav-link text-white" href="/views/user/login.php">로그인</a>
                 <a class="nav-link text-white" href="#">회원가입</a>
             </div>
         </div>
