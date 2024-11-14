b01435ce22045310e2e2f08b83dfcd9946c49630
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-15 02:26:40 +0900

    fix: 쇼핑몰 링크 범위, login footer 경로 수정

--- views/home/header.php
+++ views/home/header.php
@@ -13,7 +13,7 @@
 <header>
     <nav class="navbar navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container d-flex justify-content-end">
-            <a class="w-100 text-center navbar-brand" href="/views/home/index.php">쇼핑몰</a>
+            <a class="navbar-brand mx-auto" href="/views/home/index.php">쇼핑몰</a>
             <div class="d-flex gap-3">
                 <a class="nav-link text-white" href="#">로그인</a>
                 <a class="nav-link text-white" href="#">회원가입</a>
--- views/user/login.php
+++ views/user/login.php
@@ -16,7 +16,7 @@
     </div>
 
     <?php
-    include '/footer.php';
+    include '../home/footer.php';
     ?>
 </body>
 
