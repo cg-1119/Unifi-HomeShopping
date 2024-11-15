df54a09046dc222002634dd7beb1874b2dfadf59
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-15 22:08:28 +0900

    design: custom-style.css 수정, agree.php 디자인, 내용 추가

--- views/user/join/agree.php
+++ views/user/join/agree.php
@@ -6,23 +6,45 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/bootstrap.css">
     <link rel="stylesheet" href="/public/css/custom-style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="inner">
-            <div class="content">
-                <form method-="GET" action="">
-                    <ul>
-                        <li>회원가입 어쩌고 저쩌고</li>
-                        <li>회원가입 후 관리자의 승인을 받아야 사이트의 모든 서비스를 이용하실 수 있습니다.</li>
-                    </ul>
-                </form>
+<div class="container">
+    <div method-="GET" action="input.php">
+        <div class="mt-5 d-flex flex-column justify-content-center">
+            <div class="border-box">
+                <input type="checkbox" id="check_all" hidden>
+                <label for="check_all" class="label-box">
+                    <i class="bi bi-check-circle" style="font-size: 1.5rem"></i>
+                    <span>전체 동의하기</span>
+                </label>
+                <ul class="list-group">
+                    <li class="list-group">
+                        <label for="termsService" class="label-box">
+                            <i class="bi bi-check-circle" style="font-size: 1.5rem"></i>
+                            <span>홈쇼핑 이용약관</span>
+                        </label>
+                    </li>
+                    <li class="list-group">
+                        <label for="termsPrivacy" class="label-box">
+                            <i class="bi bi-check-circle" style="font-size: 1.5rem"></i>
+                            <span>개인정보 수집동의</span>
+                        </label>
+                    </li>
+                </ul>
+            </div>
+            <div class="box">
+                <div style="padding:24px; width: 100%; flex-direction: column;">
+                    <input class="form-control" type="submit" value="다음으로">
+                </div>
             </div>
         </div>
-
+        </form>
     </div>
+</div>
+
     <?php
     include "../../home/footer.php";
     ?>
--- views/user/login.php
+++ views/user/login.php
@@ -11,31 +11,30 @@
 
 <body>
 
-    <div class="mt-5 d-flex justify-content-center">
-        <h1>로그인</h1>
-    </div>
-    <div class="d-flex justify-content-center login-box">
-        <div style="padding:24px; width: 100%;">
-            <input type="text" class="mb-4 form-control " name="" id="" placeholder="이름" />
-            <input type="text" class="mb-4 form-control" name="" id="" placeholder="비밀번호" />
-            <input type="submit" class="form-control" value="로그인">
-            <div class="mt-2 d-flex justify-content-center" style="width: 100%">
-                <a class="nav-link text-secondary" href="join/agree.php">회원가입</a>
-                <span class="link-border"></span>
-                <a class="nav-link text-secondary" href="#">아이디 찾기</a>
-                <span class="link-border"></span>
-                <a class="nav-link text-secondary" href="#">비밀번호 찾기</a>
-            </div>
-            <div class="mt-5 text-bg-danger" id="id_error_msg" style="display: none">아이디를 잘못 입력하셨습니다.</div>
-            <div class="mt-5 text-bg-danger" id="id_error_msg" style="display: none">비밀번호를 잘못 입력하셨습니다.</div>
+<div class="mt-5 d-flex justify-content-center">
+    <h1>로그인</h1>
+</div>
+<div class="d-flex justify-content-center border-box">
+    <div style="padding:24px; width: 100%;">
+        <input type="text" class="mb-4 form-control " name="username" id="" placeholder="이름"/>
+        <input type="password" class="mb-4 form-control" name="password" id="" placeholder="비밀번호"/>
+        <input type="submit" class="form-control" value="로그인">
+        <div class="mt-2 d-flex justify-content-center" style="width: 100%">
+            <a class="nav-link text-secondary" href="join/agree.php">회원가입</a>
+            <span class="link-border"></span>
+            <a class="nav-link text-secondary" href="#">아이디 찾기</a>
+            <span class="link-border"></span>
+            <a class="nav-link text-secondary" href="#">비밀번호 찾기</a>
         </div>
+        <div class="mt-5 text-bg-danger" id="id_error_msg" style="display: none">아이디를 잘못 입력하셨습니다.</div>
+        <div class="mt-5 text-bg-danger" id="pw_error_msg" style="display: none">비밀번호를 잘못 입력하셨습니다.</div>
     </div>
+</div>
 
 
-
-    <?php
-    include '../home/footer.php';
-    ?>
+<?php
+include '../home/footer.php';
+?>
 </body>
 
 </html>
