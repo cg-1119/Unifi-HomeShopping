6254815f549d90dca50e0c345bb298caaa961c7a
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-11-27 03:38:50 +0900

    chore: 사용하지 않는 소스코드 삭제

--- models/Login.php
+++ models/Login.php
@@ -39,29 +39,5 @@ class Login
             return null;
         }
     }
-
-    // Admin 로그인 함수
-    /*public function adminLogin($id, $password) {
-        $con = $this->db->connect();
-
-        // SQL Injection 방지를 위한 Prepared Statement 사용
-        $hashedPassword = md5($password); // 비밀번호 해싱
-        $stmt = $con->prepare("SELECT * FROM admins WHERE id = ? AND pw = ?");
-        $stmt->bind_param("ss", $id, $hashedPassword); // 파라미터 바인딩
-
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        if ($result->num_rows > 0) {
-            $admin = $result->fetch_assoc();
-            $stmt->close();
-            $this->db->close();
-            return $admin; // 관리자 정보 반환
-        } else {
-            $stmt->close();
-            $this->db->close();
-            return null; // 인증 실패
-        }
-    }*/
 }
 ?>
