0a1d922ae0628e9affc6b1bd6ee992f2be27eab9
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2024-12-08 07:18:56 +0900

    refactor: 주문 관리 경로 수정, 텍스트 변경

--- views/admin/index.php
+++ views/admin/index.php
@@ -21,7 +21,7 @@ session_start();
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">상품 등록</h5>
-                    <p class="card-text">카테고리, 상품명, 가격, 상품 설명 내용을 DB에 등록</p>
+                    <p class="card-text">카테고리, 상품명, 가격, 상품 설명 내용 등록</p>
                     <a href="/views/admin/add_product.php" class="btn btn-primary">상품 등록</a>
                 </div>
             </div>
@@ -29,9 +29,9 @@ session_start();
         <div class="col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">주문내역 조회</h5>
-                    <p class="card-text">주문 완료 처리된 리스트를 DB에서 가져와 출력</p>
-                    <a href="/views/admin/order_list.php" class="btn btn-primary">주문내역 조회</a>
+                    <h5 class="card-title">주문내역 관리</h5>
+                    <p class="card-text">주문 배송 상태 처리 및 취소된 주문 처리</p>
+                    <a href="/views/admin/order_management.php" class="btn btn-primary">주문 관리</a>
                 </div>
             </div>
         </div>
@@ -39,7 +39,7 @@ session_start();
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">회원 관리</h5>
-                    <p class="card-text">DB에서 회원 리스트 출력</p>
+                    <p class="card-text">회원 정보 조회 및 관리</p>
                     <a href="/views/admin/manage_users.php" class="btn btn-primary">회원 관리</a>
                 </div>
             </div>
